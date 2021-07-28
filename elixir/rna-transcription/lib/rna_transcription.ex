3ebcf78f2961346cb46d32425a3e5c5ec2a37adb
Kfcaio <caio96@tuta.io>
2021-07-27 21:47:23 -0300

    solve rna-transcription

---
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -0,0 +1,21 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    conversions = %{
+      ?G => ?C,
+      ?C => ?G,
+      ?T => ?A,
+      ?A => ?U
+    }
+
+    Enum.map(dna, fn x -> conversions[x] end)
+  end
+end
